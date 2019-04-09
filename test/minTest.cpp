c7dc6dd3a20c321a28de6c8ed88ee3730a295131
Camila Duarte <44905597+camiladuartes@users.noreply.github.com>
2019-04-09 14:55:12 -0300

    Updating project

--- test/minTest.cpp
+++ test/minTest.cpp
@@ -22,14 +22,14 @@ void print_floats(float *first, float *last){
 	std::cout << "]";
 }
 bool compare_floats(const void *va, const void *vb){
-	//COnverter o ponteiro genérico para o tipo esperado.
+	//Converter o ponteiro genérico para o tipo esperado.
 	const float *a = (const float *) va;
 	const float *b = (const float *) vb;
 	// ... de maneira de que agora é possível desrreferenciar o ponteiro.
 	return *a < *b;
 }
 bool compare_ints(const void *va, const void *vb){
-		//COnverter o ponteiro genérico para o tipo esperado.
+	//Converter o ponteiro genérico para o tipo esperado.
 	const int *a = (const int *) va;
 	const int *b = (const int *) vb;
 	// ... de maneira de que agora é possível desrreferenciar o ponteiro.
@@ -52,91 +52,3 @@ int main () {
 
 	return 0;
 }
-
-/*
-	int Vet[] = {1,2,-3,-4,5,-6};
-	scalar_multiplication(begin(Vet), end(Vet), 3);
-	for(auto i(begin(Vet)); i!= end(Vet); ++i){
-		cout << *i << " ";
-	}
-	cout << std::endl;
-*/
-
-/*
-	int Vet[] = {1, 3, -5, 4, -2, -1};
-	auto result = dot_product(begin(Vet), begin(Vet)+3, begin(Vet)+3);
-	cout << ">>> O resultado é: " << result << std::endl;
-*/
-
-/*
-	int A[] = {1,2,3,4,5};
-	int B[] = {0,0,0};
-
-	auto b_last = copy(cbegin(A)+1, cbegin(A)+4, begin(B));
-	for(auto i(begin(B)); i!= b_last; ++i){
-		cout << *i << " ";
-	}
-	cout << std::endl;
-
-	return 0;
-*/
-
-/*
-		int A[] = {-2, -8, 2, 7, -3, 10, 1, 0, -3, 7};
-
-	auto new_last = compact(begin(A), end(A));
-	for(auto i(begin(A)); i!= new_last; ++i){
-		cout << *i << " ";
-	}
-	cout << std::endl;
-
-	return 0;
-*/
-
-/*
-	int A[] = {1,2,1,2,3,3,1,2,4,5,3,4,5};
-
-	auto last = unique(begin(A), end(A));
-	for(auto i(begin(A)); i!= end(A); ++i){
-		cout << *i << " ";
-	}
-	cout << std::endl;
-
-	return 0;
-*/
-
-/*
-	enum ball_t {B = 0, W = 1};
-
-	int main () {
-
-		ball_t A[] = {W, B, B, W, W, B, W};
-
-		auto result = sort_marbles(begin(A), end(A));
-		for(auto i(begin(A)); i!= result; ++i){
-			cout << *i << " ";
-		}
-		cout << std::endl;
-
-	}
-*/
-
-/*
-	int A[] = {-5, 7, 10, 7, 8, 9, 1, 7, -2, 3};
-	int *pivot = &A[3];
-	partition(begin(A), end(A), pivot);
-	for(auto i(begin(A)); i!= end(A); ++i){
-		cout << *i << " ";
-	}
-	cout << std::endl;
-*/
-
-/*
-	value_type A[]{ 1, 2, 3, 4, 5, 6 };
-
-	rotate( std::begin(A), std::begin(A)+5, std::end(A) );
-	for(auto i(begin(A)); i!= end(A); ++i){
-		cout << *i << " ";
-	}
-	cout << std::endl;
-*/

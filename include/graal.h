5711adf61f4e2eeda909fba5266830834d4c22a6
Camila Duarte <44905597+camiladuartes@users.noreply.github.com>
2019-04-20 10:26:24 -0300

    Final project

--- include/graal.h
+++ include/graal.h
@@ -10,8 +10,10 @@
 #include <algorithm> // swap
 #include <cstdlib>
 #include <array>
+#include <cstring>
 
 namespace graal {
+
     using byte = unsigned char;
     //===Function prototypes.
 
@@ -37,7 +39,7 @@ namespace graal {
     */
     void *copy(const void *first, const void *last, const void *d_first, size_t size);
 
-    ///Clone
+    /// Clone
     /*!
     * Receives a range defined over an array and returns a
     * pointer to a new array containing a copy of the 
@@ -46,7 +48,7 @@ namespace graal {
     */
     void *clone(const void *first, const void *last, size_t size);
 
-    ///Find_if
+    /// Find_if
     /*!
     * Search for the first element equal to the predicate.
     * \return A pointer to the first element that is equal to 
@@ -56,7 +58,7 @@ namespace graal {
     const void *find_if(const void *first, const void *last, size_t size, Predicate p);
 
 
-    ///Find
+    /// Find
     /*!
     * Search for the first element in the range that is equal
     * to the value passed to the function.
@@ -66,59 +68,71 @@ namespace graal {
     using Equal = bool(*)(const void *, const void *);
     const void *find(const void *first, const void *last, size_t size, const void *value, Equal eq);
 
-    ///all_of
+    /// All_of
     /*!
-    * Receive a generic range [first, last] and a predicate p
+    * Receive a generic range [first, last] and a predicate p.
     * \return True if the predicate return true for all
     * elements in the range.
     */
     bool all_of(const void *first, const void *last, size_t size, Predicate p);
-    ///any_of
+    /// Any_of
     /*!
-    * Receive a generic range [first, last] and a predicate p 
+    * Receive a generic range [first, last] and a predicate p.
     * \return True if the predicate return true for at least
     * one element in the range.
     */
     bool any_of(const void *first, const void *last, size_t size, Predicate p);
 
-    ///none_of
+    /// None_of
     /*!
-    * Receive a generic range [first, last] and a predicate p
+    * Receive a generic range [first, last] and a predicate p.
     * \return True if the predicate return true for none
     * of elements in the range.
     */
     bool none_of(const void *first, const void *last, size_t size, Predicate p);
 
-    ///equalFirstVersion
+    /// Equal
     /*!
-    * Receive a range [first1;last1] and a pointer to other range first2
+    * Receive a range [first1;last1] and a pointer to other range first2.
     * \return True if the first range is equal to the range
-    * [first2;first2+(last1-first1)]
+    * [first2;first2+(last1-first1)].
     */
     bool equal(const void *first1, const void *last1, const void *first2, size_t size, Equal eq);
 
-    ///equalSecondVersion
+    /// Equal
     /*!
-    * Receive two ranges: [first1;last1] and [first2;last2]
-    * \return True if the elements in the ranges are equal
+    * Receive two ranges: [first1;last1] and [first2;last2].
+    * \return True if the elements in the ranges are equal.
     */
     bool equal(const void *first1, const void *last1, const void *first2, const void *last2, size_t size, Equal eq);
 
     /// Unique
     /*!
-    * Eliminates element repetitions in range [first;last)
+    * Eliminates element repetitions in range [first;last).
     * \return A pointer to the adress just past the last element of
-    * the range with unique elements
+    * the range with unique elements.
     */
     void *unique(void *first, void *last, size_t size, Equal eq);
 
-    // /// Partition
-    // /*!
-    //  * Put the vector in order by a pivot. Elements smaller than the 
-    //  * pivot appear before, elements equal to the pivot in the middle 
-    //  * and the largest in the end.
-    //  */
-    // void partition(value_type *first, value_type *last, value_type *pivot);
+    /// Partition
+    /*!
+     * Put the vector in order by a pivot. Elements smaller than the 
+     * pivot appear before, elements equal to the pivot in the middle 
+     * and the largest in the end.
+     * \return A pointer to the begining of the range for which
+     * predicate p return false.
+     */
+    void *partition(void *first, void *last, size_t size, Predicate p);
+
+    /// Qsort
+    /*!
+     * Receives and sorts the elements in ascending order. Also
+     * receives a function pointer to a comparison function
+     * that receives two generic pointers "a" and "b" return true if a < b. 
+     */
+    void *partition(void *first, void *last, void *pivot, size_t size, Compare cmp);
+    void qsort(void *first, void *last, size_t size, Compare cmp);
+
 }
 
 #endif

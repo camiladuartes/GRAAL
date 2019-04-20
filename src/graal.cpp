f9f77ed128a4cc0132d2da5602c4b8c89fa83a4d
Camila Duarte <44905597+camiladuartes@users.noreply.github.com>
2019-04-20 10:27:23 -0300

    Final project

--- src/graal.cpp
+++ src/graal.cpp
@@ -22,13 +22,12 @@ void graal::reverse(void *first, void *last, size_t size){
     byte *plast = (byte *)last;
     byte *aux = new byte[size];
     plast -= size;
-    while(pfirst != plast){
+    while(pfirst < plast){
         std::memcpy(aux, pfirst, size); //int aux = *pfirst+i;
         std::memcpy(pfirst, plast, size); //*pfirst+i = *plast-i-1;
         std::memcpy(plast, aux, size); //*plast-i-1 = aux;
         pfirst += size;
         plast -= size;
-        // std::swap(*(first+i), *(last-i-1));
     }
 }
 
@@ -176,7 +175,9 @@ bool graal::equal(const void *first1, const void *last1, const void *first2, con
 
 void *graal::unique(void *first, void *last, size_t size, Equal eq){
     byte *clone = (byte *)first; //para conferir com cada valor anterior no for
+    // value_type *clone = first; //para conferir com cada valor anterior no for
     byte *aux = (byte *)first; //será o novo last. Possui o local do próximo elemento candidato
+    // value_type *aux = first; //será o novo last. Possui o local do próximo elemento candidato
     int cont = 0; //para saber em que indice o first(que anda pelo vetor) está
     byte *pfirst = (byte *)first;
     byte *plast = (byte *)last;
@@ -199,4 +200,63 @@ void *graal::unique(void *first, void *last, size_t size, Equal eq){
         cont++;
     }
     return aux;
+}
+
+void *graal::partition(void *first, void *last, size_t size, Predicate p){
+    byte *aux = (byte *)first; // Marca a região dos menores-que-o-pivot    
+    byte *pfirst = (byte *)first;
+    byte *plast = (byte *)last;
+    while(pfirst < plast-size){
+        if(p(pfirst)){
+            //Sim, então devemos lançar o elemento na região dos menores:
+            byte temp[size]; //variável auxiliar pra fazer o swap
+            std::memcpy(temp, aux, size); //std::swap(*aux, *pfirst);
+            std::memcpy(aux, pfirst, size);
+            std::memcpy(pfirst, temp, size);
+            //Não esquecer de aumentar a região dos menores-que-o-pivot:
+            aux += size; //Avança quando faz swap com fast quando fast < pivot
+        }
+        pfirst += size; //first sempre é incrementado a cada iteração. Sempre move.
+    }
+    byte temp2[size];
+    std::memcpy(temp2, (plast-size), size); // std::swap(*(plast-size), *aux);
+    std::memcpy((plast-size), aux, size);
+    std::memcpy(aux, temp2, size);
+    return aux;
+}
+
+void *graal::partition(void *first, void *last, void *pivot, size_t size, Compare cmp){
+    byte *aux = (byte *)first; // Marca a região dos menores-que-o-pivot    
+    byte *pfirst = (byte *)first;
+    byte *plast = (byte *)last;
+    byte *p_pivot  = (byte *)pivot;
+    byte temp[size]; //variável auxiliar pra fazer o swap
+    std::memcpy(temp, p_pivot, size); //std::swap(*aux, *pfirst);
+    std::memcpy(p_pivot, plast-size, size);
+    std::memcpy(plast-size, p_pivot, size);
+    while(pfirst != plast){
+        if(cmp(pfirst, plast-size)){
+            //Sim, então devemos lançar o elemento na região dos menores:
+            std::memcpy(temp, aux, size); //std::swap(*aux, *pfirst);
+            std::memcpy(aux, pfirst, size);
+            std::memcpy(pfirst, temp, size);
+            //Não esquecer de aumentar a região dos menores-que-o-pivot:
+            aux += size; //Avança quando faz swap com fast quando fast < pivot
+        }
+        pfirst += size; //first sempre é incrementado a cada iteração. Sempre move.
+    }
+    std::memcpy(temp, (plast-size), size); // std::swap(*(plast-size), *aux);
+    std::memcpy((plast-size), aux, size);
+    std::memcpy(aux, temp, size);
+    return aux;
+}
+
+void graal::qsort(void *first, void *last, size_t size, Compare cmp){
+    byte *pfirst = (byte *)first; 
+    byte *plast = (byte *)last;
+    if(pfirst < plast){
+        byte *pivot = (byte *)graal::partition(pfirst, plast, plast-size, size, cmp);
+        graal::qsort(pfirst, pivot, size, cmp);
+        graal::qsort(pivot+size, plast, size, cmp);
+    }
 }
